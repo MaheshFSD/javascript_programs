cf9829426f29a2e05131a11f543a8854a084a1d6
Mahesh G <maheshg@Maheshs-MacBook-Pro.local>
2024-01-03 04:12:31 +0530

    exploring console methods

---
+++ javascript.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+console.warn("Hello Warnijng")
+console.error("Hello Error");
+console.table({name:"MAhesh", age: 20});
+console.log("clearing the console");
+
+
+// variables 
+let a;
+console.log(a); 
+console.log("hello"); 
+console.log("first");
+console.clear();
+
+console.log("hello"); 
+console.log("Second");
+
